740dd5ba630acf9a19a5474afdbd2de70719694e
Gabriela S. Soria <soria.gaby@gmail.com>
2016-09-20 21:13:34 -0400

    segundo parcial clase Funcion

---
+++ src/py/edu/uaa/pooj/primerparcial/fila2/cines/Funcion.java
@@ -0,0 +1,40 @@
+package py.edu.uaa.pooj.primerparcial.fila2.cines;
+
+import java.util.Date;
+/**
+ * 
+ * @author gsoria
+ * @
+ *
+ */
+public class Funcion {
+	private Pelicula pelicula;
+	private Sala sala;
+	private Date horario;
+	
+	public Pelicula getPelicula() {
+		return pelicula;
+	}
+	public void setPelicula(Pelicula pelicula) {
+		this.pelicula = pelicula;
+	}
+	public Sala getSala() {
+		return sala;
+	}
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+	public Date getHorario() {
+		return horario;
+	}
+	public void setHorario(Date horario) {
+		this.horario = horario;
+	}
+	
+	public void agregarFuncion(){
+		
+	}
+
+	
+
+}
---
+++ src/py/edu/uaa/pooj/primerparcial/fila2/cines/Sala.java
@@ -0,0 +1,5 @@
+package py.edu.uaa.pooj.primerparcial.fila2.cines;
+
+public class Sala {
+
+}
