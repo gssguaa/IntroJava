f90ec335b09a7a6e34a7ff7e63daa8b76157e594
Gabriela Soria <soria.gaby@gmail.com>
2016-10-18 21:28:04 -0300

    test unitario

---
+++ src/py/edu/uaa/pooj/segundoparcial/fila1/CuentaCorrrienteTest.java
@@ -0,0 +1,26 @@
+package py.edu.uaa.pooj.segundoparcial.fila1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CuentaCorrrienteTest {
+
+	//TODO debitarAprobado
+	@Test
+	public void debitarAprobado() {
+		fail("Not yet implemented");
+	}
+
+	//TODO debitarRechazado
+	@Test
+	public void debitarRechazado() {
+		fail("Not yet implemented");
+	}
+
+	//TODO acreditar
+	@Test
+	public void acreditar() {
+		fail("Not yet implemented");
+	}
+}
